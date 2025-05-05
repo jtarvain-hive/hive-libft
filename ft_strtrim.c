c141cfc48432724b57c8ed4fc696fdfc019cf9b2
jtarvain <jtarvain-hive@gmail.com>
2025-05-05 22:20:20 +0300

    debug tomorrow

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jtarvain <jtarvain@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 12:16:34 by jtarvain          #+#    #+#             */
-/*   Updated: 2025/05/05 15:55:22 by jtarvain         ###   ########.fr       */
+/*   Updated: 2025/05/05 22:19:50 by jtarvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,15 @@ char	*ft_strtrim(const char *s1, const char *set)
 	start = 0;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	end = ft_strlen(s1) - 1;
+	end = ft_strlen(s1);
 	while (end > start && ft_strchr(set, s1[end]))
 		end--;
 	s2 = (char *)malloc((end - start + 1) * sizeof(char));
 	if (!s2)
 		return (NULL);
 	i = 0;
-	while (start <= end)
-	{
+	while (start < end)
 		s2[i++] = s1[start++];
-	}
 	s2[i] = '\0';
 	return (s2);
 }
